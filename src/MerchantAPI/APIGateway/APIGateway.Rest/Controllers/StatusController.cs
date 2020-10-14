bb0f9af0360c460c1612d2b848c98c6854915313
Boštjan Vižin <bostjan.vizin@crea.si>
2020-10-14 15:14:16 +0200

    Added status controller with zmq status method.

---
+++ src/MerchantAPI/APIGateway/APIGateway.Rest/Controllers/StatusController.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2020 Bitcoin Association
+
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using MerchantAPI.APIGateway.Rest.Swagger;
+using MerchantAPI.APIGateway.Rest.ViewModels;
+using MerchantAPI.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MerchantAPI.APIGateway.Rest.Services;
+using MerchantAPI.APIGateway.Domain.Models;
+
+namespace MerchantAPI.APIGateway.Rest.Controllers
+{
+  [Route("api/v1/[controller]")]
+  [ApiController]
+  [Authorize]
+  [ApiExplorerSettings(GroupName = SwaggerGroup.Admin)]
+  public class StatusController : ControllerBase
+  {
+    INodes nodes;
+    ZMQSubscriptionService subscriptionService;
+
+    public StatusController(
+      INodes nodes,
+      ZMQSubscriptionService subscriptionService
+      )
+    {
+      this.nodes = nodes ?? throw new ArgumentNullException(nameof(nodes));
+      this.subscriptionService = subscriptionService ?? throw new ArgumentNullException(nameof(subscriptionService));
+    }
+
+    /// <summary>
+    /// Get zmq subscriptions status
+    /// </summary>
+    /// <returns>Zmq subscription status for all nodes.</returns>
+    [HttpGet]
+    [Route("zmq")]
+    public ActionResult<IEnumerable<ZmqStatusViewModelGet>> ZmqStatus()
+    {
+      var result = nodes.GetNodes();
+      return Ok(result.Select(n => new ZmqStatusViewModelGet(n, subscriptionService.GetStatusForNode(n))));
+    }
+  }
+}
---
+++ src/MerchantAPI/APIGateway/APIGateway.Rest/ViewModels/ZmqEndpointViewModelGet.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2020 Bitcoin Association
+
+using System;
+using System.Text.Json.Serialization;
+using MerchantAPI.APIGateway.Domain.Models.Zmq;
+
+namespace MerchantAPI.APIGateway.Rest.ViewModels
+{
+  public class ZmqEndpointViewModelGet
+  {
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    [JsonPropertyName("topic")]
+    public string[] Topics { get; set; }
+
+    [JsonPropertyName("lastPingAt")]
+    public DateTime LastPingAt { get; set; }
+
+    [JsonPropertyName("lastMessageAt")]
+    public DateTime? LastMessageAt { get; set; }
+
+
+    public ZmqEndpointViewModelGet(ZmqEndpoint domain)
+    {
+      Address = domain.Address;
+      Topics = domain.Topics;
+      LastPingAt = domain.LastPingAt.ToUniversalTime();
+      LastMessageAt = domain.LastMessageAt?.ToUniversalTime();
+    }
+  }
+}
