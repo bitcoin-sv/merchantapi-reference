6e771781c5345bf318f9318052a536a3a3439c5f
KarmenK <karmen.knavs@crea.si>
2020-10-28 09:29:49 +0100

    Added fee amount type to separate mining and relay fees.

--- src/MerchantAPI/APIGateway/APIGateway.Domain/MerchantAPI.APIGateway.Domain/Models/FeeAmount.cs
+++ src/MerchantAPI/APIGateway/APIGateway.Domain/MerchantAPI.APIGateway.Domain/Models/FeeAmount.cs
@@ -6,19 +6,41 @@ using System.Text.Json.Serialization;
 
 namespace MerchantAPI.APIGateway.Domain.Models
 {
-  public class FeeAmount : IValidatableObject
+  public class FeeAmount : IValidatableObject 
   {
-    [JsonPropertyName("satoshis")]
+
+    public static class AmountType
+    {
+      public const string MiningFee = "MiningFee";
+      public const string RelayFee = "RelayFee";     
+    }
+
+    public string FeeAmountType { get; set; }
+
     public long Satoshis { get; set; }
 
-    [JsonPropertyName("bytes")]
     public long Bytes { get; set; }
 
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    public bool IsMiningFee()
+    {
+      return FeeAmountType == AmountType.MiningFee;
+    }
+
+    public bool IsRelayFee()
+    {
+      return FeeAmountType == AmountType.RelayFee;
+    }
+
+
+    public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
       if (Satoshis < 0 || Bytes < 0)
       {
         yield return new ValidationResult($"FeeAmount: value for {nameof(Satoshis)} and {nameof(Bytes)} must be non negative.");
+      }
+      if (string.IsNullOrEmpty(FeeAmountType))
+      {
+        yield return new ValidationResult($"FeeAmount: {nameof(FeeAmountType)} is undefined.");
       }
     }
   }
