c96a21e26a357488982a903cc19cf3083db73d65
Boštjan Vižin <bostjan.vizin@crea.si>
2021-05-05 08:02:40 +0200

    Build scripts cleanup.

--- src/Deploy/build.sh
+++ src/Deploy/build.sh
@@ -1,56 +1,16 @@
 #!/bin/bash
 
-cd "$(dirname $0)"
+read -r VERSIONPREFIX<version_mapi-common.txt
 
-VERSION="${1}"
-
-if [ "${VERSION}" == "" ] ; then
-  echo "Usage: ${0} <version>"
-  exit 1
-fi
-
-TAG="v${VERSION}"
-
-
-echo "* Checking if working directory is clean"
-if [ ! -z "$(git status --untracked-files=no --porcelain)" ] ; then
-  # Uncommitted changes in tracked files
-  echo "Uncommited changes present. Exiting."
-  exit -1
-fi
-# Working directory clean excluding untracked files
-
-echo "* Fetching tags"
 git remote update
 git pull
-git fetch --tags
-
-# CURRENT_TAGS=$(git tag -l --contains HEAD)
-
-echo "* Trying to checkout tag"
-
-if git checkout ${TAG} ; then
-  echo "* Checked out branch with ${TAG}."
-else
-  echo "* Checking out branch ${TAG} failed."
-  echo "* Tagging current branch with tag: ${TAG}."
-  git tag -a ${TAG} -m "Version ${VERSION}."
-fi
-
-VERSIONPREFIX="${VERSION}"
+git status -uno
 
 COMMITID=$(git rev-parse --short HEAD)
-echo "* Using commit with id: $COMMITID"
-echo "* Last commit:"
 
-git log -n 1
+APPVERSIONMAPICOMMON="$VERSIONPREFIX-$COMMITID"
 
-echo "***************************"
-echo "***************************"
-echo "Building MerchantAPI.Common version $VERSIONPREFIX"
-read -p "Continue if you have latest version (commit $COMMITID) or terminate job and get latest files."
-
-git push origin ${TAG}
+mkdir -p Build
 
 echo "* Building NuGet MerchantAPI.Common package"
 
@@ -58,5 +18,3 @@ dotnet restore -p:Configuration=Release ..
 VERSION=${VERSIONPREFIX} dotnet build -c Release -o Build ../MerchantAPI.Common.sln
 VERSION=${VERSIONPREFIX} dotnet pack -c Release -o Build ../MerchantAPI.Common.sln
 
-echo "* Checking out master branch"
-git checkout master
